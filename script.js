6f0f8705adf8888c6f2da01af56e14153cac86b9
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-08-06 22:37:40 +0500

    add functionality of loading-bar,error,profile and some css

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const searchBtn=document.querySelector("#search-btn")
 const searchInput=document.querySelector("#search")
+const profile=document.querySelector("#profile")
+const loading=document.querySelector(".loading")
+const errorEle=document.querySelector(".error")
+let isBool=false;
 searchBtn.addEventListener("click",function(){
     if(searchInput.value==""){
         alert("please give a user name to search")
@@ -8,26 +12,43 @@ searchBtn.addEventListener("click",function(){
     getUser()
 
 })
-async function getUser() {
 
+
+
+async function getUser() {
     const name=searchInput.value
 const url=`https://api.github.com/users/${name}`
 try {
+    if(!isBool){
+        loading.style.display="block"
+    }
+    errorEle.textContent=""
    const response=await fetch(url)
-   if(!response.ok){
-    throw new Error("wrong baba")
-
-
-   }
 const data=await response.json()
+isBool=true
+
+if(isBool){
+        loading.style.display="none"
+        isBool=false
+}
+if(!response.ok){
+    throw new Error("User not found with this name")
+}
 console.log(data) 
+
+profile.innerHTML=`
+<img class="profile-img" src=${data.avatar_url} >
+
+`
 } catch (error) {
-    console.log("wrong")
-    console.log(error.message)
-    
+   errorEle.textContent=error.message
 }
 
 
 
     
+}
+const getProfile=(data)=>{
+    
+
 }
